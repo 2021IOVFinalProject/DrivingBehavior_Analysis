63ee5258b343d033f4b853eefda97a11a4908cae
Dick Hansel Ryan <hanselryan77@gmail.com>
2021-10-15 22:03:34 +0800

    Updated code

--- Plot Module/EDA_module.py
+++ Plot Module/EDA_module.py
@@ -6,40 +6,40 @@ import pandas as pd
 import seaborn as sb
 
 #Open dataset
-df = pd.read_csv('./MachineLearning/User1_Dataset.csv', index_col = [1])
+df = pd.read_csv('./MachineLearning/User2_Dataset.csv', index_col = [1])
 df.drop(df.columns[df.columns.str.contains('Unnamed')], axis = 1, inplace = True)
 df.drop('Driver Type', axis = 1, inplace = True)
-print(df.columns)
+#print(df.columns)
 
 #Print top rows of dataset
-#print(df.head())
+print(df.head())
 
 #Print shape of dataset
-#print(df.shape)
+print(df.shape)
 
 #Descriptive statistics
-#print(df.describe())
+print(df.describe())
 
 #Skew of Data
-#print(df.skew())
+print(df.skew())
 
 #Show histogram bar of all features in dataset to show the number of population
 df.hist()
 plt.show()
 
 #Density plot to get a quick idea of the distribution of each attribute
-# df.plot(kind = 'density', subplots = True, layout = (3,3), sharex = False)
-# plt.show()
+df.plot(kind = 'density', subplots = True, layout = (3,3), sharex = False)
+plt.show()
 
 #Print correlation between columns in dataset
 # print(df.corr(method = 'pearson'))
 
 #Show heatmap of feature correlation
-# dfcorr = df.corr()
-# sb.heatmap(dfcorr, cmap = 'YlGnBu', annot = True)
-# plt.title("Correlation Heatmap")
-# plt.tight_layout()
-# plt.show()
+dfcorr = df.corr()
+sb.heatmap(dfcorr, cmap = 'YlGnBu', annot = True)
+plt.title("Correlation Heatmap")
+plt.tight_layout()
+plt.show()
 
 #Show relation between some features using regression plot
 sb.regplot(x = 'Fuel used (L)', y = 'Distance travelled (km)', data = df, color = 'navy')
