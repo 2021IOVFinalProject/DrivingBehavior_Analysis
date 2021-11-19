4445a4873d1ff03568a664f2cb7e5e035617592e
Dick Hansel Ryan <hanselryan77@gmail.com>
2021-11-19 18:04:50 +0800

    Update Code

--- Plot Module/plot_insuranceTotaldistance.py
+++ Plot Module/plot_insuranceTotaldistance.py
@@ -5,13 +5,21 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('./MachineLearning/DrivingBehavior_Dataset.csv', index_col = [1], parse_dates = ['Date'])
 print(df.columns)
 
+#Resample dataset
 dist_resample = df.resample('M').sum()['Distance_travelled(km)']
 print(dist_resample)
 
-# ax = dist_resample.plot.bar(color = 'darkblue')
-# ax.set_xticklabels(dist_resample.index.strftime('%Y-%m-%d'))
-# plt.xlabel('Month')
-# plt.ylabel('Total Distance (km)')
-# plt.title('Total Distance Driven per Month')
-# plt.tight_layout()
-# plt.show()
+ax = dist_resample.plot.bar(color = 'darkblue')
+ax.set_xticklabels(dist_resample.index.strftime('%Y-%m-%d'))
+plt.xlabel('Month')
+plt.ylabel('Total Distance (km)')
+plt.title('Total Distance Driven per Month')
+plt.tight_layout()
+plt.show()
+
+#Convert dist_resample to dataframe
+new_df = pd.DataFrame(data = dist_resample)
+print(new_df.head())
+
+#Export to new dataset
+new_df.to_csv('./MachineLearning/Insurance_DrivingDistance_Dataset.csv', index = True)
